398531c8f890c98e5fe7fe615dc09f98d1f6f93d
Elizabeth <foxcapade@gmail.com>
2022-02-08 10:58:26 -0500

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,85 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
+plugins {
+  `java-library`
+  `maven-publish`
+  kotlin("jvm") version "1.6.10"
+  id("org.jetbrains.dokka") version "1.6.10"
+}
+
+group = "org.veupathdb.lib"
+version = "1.0.0"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib"))
+  implementation("io.prometheus:simpleclient:0.14.1")
+  implementation("io.prometheus:simpleclient_common:0.14.1")
+}
+
+kotlin {
+  jvmToolchain {
+    (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(8))
+  }
+}
+
+java {
+  withJavadocJar()
+  withSourcesJar()
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+  kotlinOptions {
+    jvmTarget = "1.8"
+    freeCompilerArgs = listOf("-Xjvm-default=all")
+  }
+}
+
+tasks.withType<DokkaTask>().configureEach {
+  dokkaSourceSets.configureEach {
+    includeNonPublic.set(false)
+    jdkVersion.set(8)
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GitHub"
+      url = uri("https://maven.pkg.github.com/VEuPathDB/lib-prometheus-jvm-stats")
+      credentials {
+        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+      }
+    }
+  }
+
+  publications {
+    create<MavenPublication>("gpr") {
+      from(components["java"])
+
+      pom {
+        name.set("Prometheus JVM Stats")
+        description.set("Reports JVM stats to the default Prometheus java instance.")
+        url.set("https://github.com/VEuPathDB/lib-prometheus-jvm-stats")
+        developers {
+          developer {
+            id.set("epharper")
+            name.set("Elizabeth Paige Harper")
+            email.set("epharper@upenn.edu")
+            url.set("https://github.com/foxcapades")
+            organization.set("VEuPathDB")
+          }
+        }
+        scm {
+          connection.set("scm:git:git://github.com/VEuPathDB/lib-prometheus-jvm-stats.git")
+          developerConnection.set("scm:git:ssh://github.com/VEuPathDB/lib-prometheus-jvm-stats.git")
+          url.set("https://github.com/VEuPathDB/lib-prometheus-jvm-stats")
+        }
+      }
+    }
+  }
+}
---
+++ src/main/kotlin/org/veupathdb/lib/prom/PrometheusJVM.kt
@@ -0,0 +1,81 @@
+package org.veupathdb.lib.prom
+
+import io.prometheus.client.Gauge
+import io.prometheus.client.Summary
+import java.lang.management.ManagementFactory
+
+/**
+ * Prometheus JVM Stats
+ *
+ * Access point to enable JVM stats for a service.
+ */
+object PrometheusJVM {
+
+  @JvmStatic
+  private val TotalMemory = Gauge.build()
+    .name("process_total_memory")
+    .create()
+
+  @JvmStatic
+  private val FreeMemory = Gauge.build()
+    .name("process_free_memory")
+    .create()
+
+  @JvmStatic
+  private val UsedMemory = Gauge.build()
+    .name("process_active_memory")
+    .create()
+
+  @JvmStatic
+  private val GCCount = Summary.build()
+    .name("gc_count")
+    .create()
+
+  @JvmStatic
+  private val GCTime = Summary.build()
+    .name("gc_time")
+    .create()
+
+  /**
+   * Enable JVM stats.
+   */
+  @JvmStatic
+  fun enable() {
+
+    Thread {
+      while (true) {
+        Thread.sleep(500)
+        calculate()
+      }
+    }
+
+  }
+
+  @JvmStatic
+  private fun calculate() {
+    val total: Long
+    val free: Long
+    val diff: Long
+
+    with(Runtime.getRuntime()) {
+      total = totalMemory()
+      free  = freeMemory()
+      diff  = total - free
+    }
+
+    TotalMemory.set(total.toDouble())
+    FreeMemory.set(free.toDouble())
+    UsedMemory.set(diff.toDouble())
+
+    var totalGC = 0L
+    var gcTime = 0L
+
+    ManagementFactory.getGarbageCollectorMXBeans().forEach {
+      totalGC += it.collectionCount
+      gcTime  += it.collectionTime
+    }
+
+    GCCount.observe(totalGC.toDouble())
+    GCTime.observe(gcTime.toDouble())
+  }
+}
